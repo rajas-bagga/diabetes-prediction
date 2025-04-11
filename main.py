a82ba8013a15407f12a94a5532016b8b58dcaad5
New Name <new.email@example.com>
2025-04-12 01:18:11 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+# Load the model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+with st.form('input_form'):
+    st.subheader("User Input")
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        gender = st.selectbox("Gender", ["Male", "Female"])
+        age = st.number_input("Age", value=20, min_value=0)
+        hypertension = st.selectbox("Hypertension", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
+        heart_disease = st.selectbox("Heart Disease", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
+        
+    
+    with col2:
+        smoking_history = st.selectbox("Smoking History", ["Never", "Former", "Current"])
+        blood_glucose_level = st.number_input("Blood Glucose Level (mg/dL)", min_value=0.0, value=100.0)
+        hba1c_level = st.number_input("HbA1c(glycated hemoglobin) Level (%)", min_value=0.0, value=5.5)
+        bmi = st.number_input("BMI", min_value=0.0, value=25.0,)
+    
+    submit_button = st.form_submit_button(label='Submit')
+    smoking_status = [0, 0, 0]
+    if submit_button:
+        smoking_status = [0, 0, 0]
+        if smoking_history.lower() == "never":
+            smoking_status[0] = 1
+        elif smoking_history.lower() == "former":
+            smoking_status[1] = 1
+        elif smoking_history.lower() == "current":
+            smoking_status[2] = 1
+    
+    input_data = pd.DataFrame([{
+            "gender": 0 if gender.lower() == "male" else 1,
+            "age": age,
+            "hypertension": hypertension,
+            "heart_disease": heart_disease,
+            "bmi": bmi,
+            "HbA1c_level": hba1c_level,
+            "blood_glucose_level": blood_glucose_level,
+            "smoking_history_current": smoking_status[2],
+            "smoking_history_former": smoking_status[1],
+            "smoking_history_never": smoking_status[0],
+        }])
+    
+    if submit_button:
+        input_scaled = scaler.transform(input_data)
+        input_scaled = pd.DataFrame(input_scaled, columns=input_data.columns)
+        prediction = model.predict(input_scaled)[0]
+        result_class = "Success" if prediction == 0 else "Error"
+        
+        if prediction == 0:
+            st.success("You don't have diabetes nigga")
+        else:
+            st.error("Nigga you have diabetes")
+
+    
---
+++ model.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+# Load and clean data
+data = pd.read_csv("diabetes_prediction_dataset.csv")
+data = data[data['smoking_history'] != 'ever']
+data = data[data['smoking_history'] != 'not current']
+data = data[data['smoking_history'] != 'No Info']
+
+# Balance the dataset
+df_majority = data[data['diabetes'] == 0]
+df_minority = data[data['diabetes'] == 1]
+df_majority_undersampled = df_majority.sample(n=len(df_minority), random_state=42)
+data = pd.concat([df_majority_undersampled, df_minority], axis=0).reset_index(drop=True)
+
+# Encode categorical variables
+data = pd.get_dummies(data, columns=['smoking_history'], dtype=int)
+data["gender"] = data["gender"].map({"Male": 0, "Female": 1})
+
+# Split features and target
+X = data.drop("diabetes", axis=1)
+Y = data["diabetes"]
+
+# Scale features
+scaler = MinMaxScaler()
+X_scaled = scaler.fit_transform(X)
+X = pd.DataFrame(X_scaled, columns=X.columns)
+
+# Split into train/test
+xtrain, xtest, ytrain, ytest = train_test_split(X, Y, test_size=0.25)
+
+
+# Train model
+model = RandomForestClassifier(max_depth=9, n_estimators=100)
+
+param_grid = {
+    'n_estimators': [100, 500],
+    'max_depth': [7, 9, 15],
+    'criterion': ['gini', 'entropy'],
+}
+
+print("Training model...")
+grid_search = GridSearchCV(model, param_grid, cv=3, n_jobs=-1, verbose=2)
+grid_search.fit(xtrain, ytrain)
+print("Model trained!")
+print("Best parameters found: ", grid_search.best_params_)
+print("Best score found: ", grid_search.best_score_)
+print(f"Accuracy Score: {grid_search.score(xtest, ytest)}")
+
+with open('model.pkl', 'wb') as file:
+    pickle.dump(grid_search, file)
+
+with open('scaler.pkl', 'wb') as file:
+    pickle.dump(scaler, file)
+
+# Input from user
+# smoking_status = [0, 0, 0]
+
+# gender = input("Enter your gender [male/female]: ").strip().lower()
+# age = int(input("Enter your age: "))
+# hypertension = int(input("Do you have hypertension? [0/1]: "))
+# heart_disease = int(input("Do you have heart disease? [0/1]: "))
+# bmi = float(input("Enter your BMI: "))
+# smoking_history = input("Enter your smoking history [never/former/current]: ").strip().lower()
+# blood_glucose_level = float(input("Enter your blood glucose level: "))
+# glycated_hemoglobin = float(input("Enter your glycated hemoglobin: "))
+
+# if smoking_history == "never":
+#     smoking_status[0] = 1
+# elif smoking_history == "former":
+#     smoking_status[1] = 1
+# elif smoking_history == "current":
+#     smoking_status[2] = 1
+
+# # Build input DataFrame
+# dataframe = pd.DataFrame([{
+#     "gender": 0 if gender == "male" else 1,
+#     "age": age,
+#     "hypertension": hypertension,
+#     "heart_disease": heart_disease,
+#     "bmi": bmi,
+#     "HbA1c_level": glycated_hemoglobin,
+#     "blood_glucose_level": blood_glucose_level,
+#     "smoking_history_current": smoking_status[2],
+#     "smoking_history_former": smoking_status[1],
+#     "smoking_history_never": smoking_status[0],
+# }])
+
+# # Scale input and predict
+# dataframe = pd.DataFrame(scaler.transform(dataframe), columns=X.columns)
+# res = grid_search.predict(dataframe)
+
+# print(f"Predicted Diabetes Status: {'Yes' if res[0] == 1 else 'No'}")
